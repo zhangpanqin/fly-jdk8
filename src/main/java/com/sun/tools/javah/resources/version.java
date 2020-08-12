30b5fb49142320ef1d4258172436b6f3e2da7946
万古云霄 <zhangpanqin@outlook.com>
2020-08-12 21:23:34 +0800

    feat:添加 openjdk  1.8 源码

---
+++ src/main/java/com/sun/tools/javah/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.javah.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "1.8.0_265-b01" },
+            { "jdk", "1.8.0_265" },
+            { "release", "1.8.0_265" },
+        };
+    }
+}
